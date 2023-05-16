70c1d3792a67dd8ca45e9527218d88f72c6482e6
Kishan lad <kishan.l@simformsolution.com>
2023-05-16 16:03:46 +0530

    TE7-T137:rails-I18n:Feat:Adding Rails I18n task.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def custom_error(model,field)
+    error_messages = model.errors.full_messages_for(field)
+    unless error_messages.empty?
+      field = model.class.human_attribute_name(field)
+      error_messages.each do |message|
+        puts "<div class='alert alert-danger'>#{message}</div>"
+      end
+    end
+  end
 end
